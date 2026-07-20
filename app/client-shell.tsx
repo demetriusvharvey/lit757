81e5f93cd582095f8ab56e1a253dd786179a68ab
Demetrius Harvey <78282284+demetriusvharvey@users.noreply.github.com>
2026-07-20 11:03:35 -0400

    Load browser-only UI components without SSR

---
+++ app/client-shell.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import dynamic from "next/dynamic";
+
+const MapLayerGuard = dynamic(() => import("./map-layer-guard"), { ssr: false });
+const MobileRenderGuard = dynamic(() => import("./mobile-render-guard"), { ssr: false });
+const LocationExperience = dynamic(() => import("./location-experience"), { ssr: false });
+const MobileHome = dynamic(() => import("./mobile-home"), { ssr: false });
+const DesktopHome = dynamic(() => import("./desktop-home"), { ssr: false });
+const VenueBrowserExperience = dynamic(() => import("./venue-browser-experience"), { ssr: false });
+const BuzzExperience = dynamic(() => import("./buzz-experience"), { ssr: false });
+const BuzzBrandEnhancer = dynamic(() => import("./buzz-brand-enhancer"), { ssr: false });
+const HomeMapResizer = dynamic(() => import("./home-map-resizer"), { ssr: false });
+const VenueDetailEnhancer = dynamic(() => import("./venue-detail-enhancer"), { ssr: false });
+const VenueVotingEnhancer = dynamic(() => import("./venue-voting-enhancer"), { ssr: false });
+const LegacyEnhancers = dynamic(() => import("./legacy-enhancers"), { ssr: false });
+
+export default function ClientShell() {
+  return (
+    <>
+      <MapLayerGuard />
+      <MobileRenderGuard />
+      <LocationExperience />
+      <MobileHome />
+      <DesktopHome />
+      <VenueBrowserExperience />
+      <BuzzExperience />
+      <BuzzBrandEnhancer />
+      <HomeMapResizer />
+      <VenueDetailEnhancer />
+      <VenueVotingEnhancer />
+      <LegacyEnhancers />
+    </>
+  );
+}
