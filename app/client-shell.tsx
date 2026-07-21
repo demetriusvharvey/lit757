143cdc86a81deb575261adba90692820ab4767c3
Demetrius Harvey <78282284+demetriusvharvey@users.noreply.github.com>
2026-07-21 15:53:19 -0400

    Make unified map the primary Buzz experience

--- app/client-shell.tsx
+++ app/client-shell.tsx
@@ -4,18 +4,9 @@ import dynamic from "next/dynamic";
 import { usePathname } from "next/navigation";
 import { MapControllerProvider } from "./map-controller";
 
-const MobileRenderGuard = dynamic(() => import("./mobile-render-guard"), { ssr: false });
-const LocationExperience = dynamic(() => import("./location-experience"), { ssr: false });
-const MobileHome = dynamic(() => import("./buzz-mobile-home"), { ssr: false });
-const BuzzPinRestorer = dynamic(() => import("./buzz-pin-restorer"), { ssr: false });
-const BuzzProductPositioning = dynamic(() => import("./buzz-product-positioning"), { ssr: false });
-const BuzzScoreTransparency = dynamic(() => import("./buzz-score-transparency"), { ssr: false });
-const BuzzCrowdReport = dynamic(() => import("./buzz-crowd-report"), { ssr: false });
-const BuzzCommunityProof = dynamic(() => import("./buzz-community-proof"), { ssr: false });
-const DesktopHome = dynamic(() => import("./buzz-desktop-home"), { ssr: false });
+const BuzzMapApp = dynamic(() => import("./buzz-map-app"), { ssr: false });
 const NotificationRuntime = dynamic(() => import("./notification-runtime"), { ssr: false });
 const NotificationAuth = dynamic(() => import("./notification-auth"), { ssr: false });
-const NotificationTestButton = dynamic(() => import("./notification-test-button"), { ssr: false });
 
 export default function ClientShell() {
   const pathname = usePathname();
@@ -25,18 +16,9 @@ export default function ClientShell() {
     <MapControllerProvider>
       {!isDistrictExperience && (
         <>
-          <MobileRenderGuard />
-          <LocationExperience />
-          <MobileHome />
-          <BuzzPinRestorer />
-          <BuzzProductPositioning />
-          <BuzzScoreTransparency />
-          <BuzzCrowdReport />
-          <BuzzCommunityProof />
-          <DesktopHome />
+          <BuzzMapApp />
           <NotificationRuntime />
           <NotificationAuth />
-          <NotificationTestButton />
         </>
       )}
     </MapControllerProvider>
